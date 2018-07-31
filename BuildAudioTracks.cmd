fafb98eb794ba7521a814421a0be0dd3181dbce4
synaptek <KevinWillows@gmail.com>
2018-07-31 14:55:19 -0400

    template wip

--- BuildAudioTracks.cmd
+++
@@ -1,3 +0,0 @@
-#This builds the audio tracks (mp3's) from the user scripts
-
-node EFAudio/dist/compiler.js
--- updateModuleGUID.cmd
+++
@@ -1,4 +0,0 @@
-#This updates the Unique ID used to identify the Animate module in the Adobe internal 
-#library
-
-node utils/GUIDGen/dist/generator.js
